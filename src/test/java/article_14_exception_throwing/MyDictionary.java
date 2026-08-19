5c73f41fc3fe976c36ae944282b87a35b8532d2e
rogerioduenas <rogerioduenasdev@hotmail.com>
2026-08-19 13:44:56 +0900

    feat: implement exception throwing using thenThrow, doThrow, and spy

--- src/test/java/article_14_exception_throwing/MyDictionary.java
+++ src/test/java/article_14_exception_throwing/MyDictionary.java
@@ -1,4 +1,16 @@
 package article_14_exception_throwing;
 
-public class MyDictionary {
+import java.util.Map;
+
+class MyDictionary {
+
+  private Map<String, String> wordMap;
+
+  public void add(String word, String meaning) {
+    wordMap.put(word, meaning);
+  }
+
+  public String getMeaning(String word) {
+    return wordMap.get(word);
+  }
 }
--- src/test/java/article_14_exception_throwing/ThrowingTest.java
+++ src/test/java/article_14_exception_throwing/ThrowingTest.java
@@ -2,23 +2,60 @@ package article_14_exception_throwing;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class Throwing {
+public class ThrowingTest {
 
+  @Mock
+  MyDictionary dictionary;
+
+  //Non-Void Return
   @Test
   void givenNonVoidReturnType_whenUsingWhenThen_thenExceptionIsThrown() {
 
-    MyDictionary dictMock = mock(MyDictionary.class);
+    when(dictionary.getMeaning(anyString())).thenThrow(NullPointerException.class);
 
-    when(dictMock.getMeaning(anyString())).thenThrow(NullPointerException.class);
+    assertThrows(NullPointerException.class, () -> dictionary.getMeaning("word"));
+  }
 
-    assertThrows(NullPointerException.class, () -> dictMock.getMeaning("word"));
+  //Void Return
+  @Test
+  void givenVoidReturnType_whenUsingDoThrow_thenExceptionIsThrown() {
+    doThrow(IllegalStateException.class).when(dictionary).add(anyString(), anyString());
+
+    assertThrows(IllegalStateException.class, () -> dictionary.add("word", "meaning"));
+  }
+
+  //Exception as an Object
+  @Test
+  void givenNonVoidReturnType_whenUsingWhenThenAndExeceptionAsNewObject_thenExceptionIsThrown() {
+    when(dictionary.getMeaning(anyString())).thenThrow(new NullPointerException("Error occurred"));
+
+    assertThrows(NullPointerException.class, () -> dictionary.getMeaning("word"));
+  }
+
+  @Test
+  void givenNonVoidReturnType_whenUsingDoThrowAndExeceptionAsNewObject_thenExceptionIsThrown() {
+    doThrow(new IllegalStateException("Error occurred")).when(dictionary)
+        .add(anyString(), anyString());
+
+    assertThrows(IllegalStateException.class, () -> dictionary.add("word", "meaning"));
+  }
+
+  //spy
+  @Test
+  void givenSpyAndNonVoidReturnType_whenUsingWhenThen_thenExceptionIsThrown() {
+    MyDictionary dict = new MyDictionary();
+    MyDictionary spy = Mockito.spy(dict);
+    doThrow(NullPointerException.class).when(spy).getMeaning(anyString());
+
+    assertThrows(NullPointerException.class, () -> spy.getMeaning("word"));
   }
 }
